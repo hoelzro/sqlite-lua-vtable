13cfdff09804e6caefef50941dc147e986ab14da
Rob Hoelz <rob@hoelz.ro>
2022-03-06 08:05:34 -0600

    Remove unused example code

--- counter.lua
+++ counter.lua
@@ -2,58 +2,6 @@ local vtable = {
   name = 'counter',
 }
 
-local counter_cursor_methods = {}
-
-function counter_cursor_methods:close()
-end
-
-function counter_cursor_methods:rowid()
-end
-
-function counter_cursor_methods:column(n)
-end
-
-function counter_cursor_methods:next()
-end
-
-function counter_cursor_methods:eof()
-end
-
-function counter_cursor_methods:filter(index_num, index_str, args)
-end
-
-local counter_vtable_methods = {}
-
-function counter_vtable_methods:disconnect()
-end
-
-function counter_vtable_methods:destroy()
-end
-
-function counter_vtable_methods:open()
-end
-
-function counter_vtable_methods.update(args, rowid)
-end
-
-function counter_vtable_methods:begin()
-end
-
-function counter_vtable_methods:sync()
-end
-
-function counter_vtable_methods:commit()
-end
-
-function counter_vtable_methods:rollback()
-end
-
-function counter_vtable_methods:rename(new_name)
-end
-
-function counter_vtable_methods:find_function(args, name)
-end
-
 -- XXX args.module_name, args.database_name, args.table_name
 function vtable.create(db, args)
   db:declare_vtab 'CREATE TABLE _ (value INTEGER NOT NULL)'
