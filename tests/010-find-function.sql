0d3ac9e40b5cd671e87418a8032f9959933d21c0
Rob Hoelz <rob@hoelz.ro>
2025-07-24 09:22:24 -0500

    Add test case for find_function

---
+++ tests/010-find-function.sql
@@ -0,0 +1,8 @@
+.load ./lua-vtable
+.testcase tests/010-find-function
+
+select lua_create_module_from_file('tests/examples/counter10.lua');
+CREATE VIRTUAL TABLE counter USING counter10();
+
+SELECT * FROM counter WHERE value MATCH 2;
+SELECT * FROM counter WHERE NOT (value MATCH 2);
